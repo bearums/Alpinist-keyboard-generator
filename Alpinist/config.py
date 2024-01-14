24806a7386bc74cce6bb79aeb716e8276ee2b268
Ben Hastings <benhastings17@gmail.com>
2024-01-14 15:24:25 +0100

    microcontroller class

---
+++ Alpinist/config.py
@@ -0,0 +1,52 @@
+from enum import IntEnum
+ 
+class Shape(IntEnum):
+    LEAN = 0
+    HULL = 1
+    
+class Config:
+    nrows : int
+    row_key_num: list
+    columnSpacing: float
+    rowSpacing: float
+    switchHoleSize: float
+    plateThickness: float
+    shape: Shape
+    screwHoleDiameter: float
+    notched_keyhole: bool
+    plateFillet: float
+        
+    caseHeight: float
+    caseGap: float
+    wallThickness: float
+    def __init__(self, row_key_num, 
+                 cs=19, rs=19, 
+                 switchHoleSize=13.9,
+                 plateThickness = 2,
+                 shape=Shape.LEAN,
+                 screwHoleDiamater= 2.4,
+                 notched_keyhole=True,
+                 plateFillet=2.5,
+                 
+                 caseHeight=22,
+                 caseGap=1.5,
+                 wallThickness=1.6,
+                ):
+        self.nrows = len(row_key_num)
+        self.row_key_num = row_key_num
+        self.columnSpacing = cs
+        self.rowSpacing = rs
+        self.plateThickness = plateThickness
+        self.screwHoleDiamater = screwHoleDiamater
+        self.angle = 0
+        self.hOffset=0
+        self.switchHoleSize = switchHoleSize
+        self.shape = shape
+        self.notched_keyhole = notched_keyhole
+        self.plateFillet = plateFillet
+        
+        self.caseHeight = caseHeight
+        self.caseGap = caseGap
+        self.wallThickness = wallThickness
+        
+        
